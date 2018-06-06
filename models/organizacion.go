b2c0c7942069d9d5bd698bc5401262aae4bf8dcb
mariaalejandra9404 <mariaalejandraortiz0401@gmail.com>
2018-06-05 22:32:38 -0500

    se añade related a modelo de organizacion

--- models/organizacion.go
+++ models/organizacion.go
@@ -49,7 +49,7 @@ func GetOrganizacionById(id int) (v *Organizacion, err error) {
 func GetAllOrganizacion(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(Organizacion))
+	qs := o.QueryTable(new(Organizacion)).RelatedSel(3)
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
