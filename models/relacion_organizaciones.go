d0e1cdcf57632325807206699e398740278e616c
mariaalejandra9404 <mariaalejandraortiz0401@gmail.com>
2018-06-06 11:59:46 -0500

    se arregla relatedSel a tabla relacion_organizaciones

--- models/relacion_organizaciones.go
+++ models/relacion_organizaciones.go
@@ -49,7 +49,7 @@ func GetRelacionOrganizacionesById(id int) (v *RelacionOrganizaciones, err error
 func GetAllRelacionOrganizaciones(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(RelacionOrganizaciones))
+	qs := o.QueryTable(new(RelacionOrganizaciones)).RelatedSel(3)
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
